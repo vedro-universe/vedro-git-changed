eef3ae4be314319034d0580a927c2d4141393d88
Nikita Tsvetkov <tsv1@fastmail.com>
2024-07-18 19:34:38 +0400

    bump version → 0.2.0

--- vedro_git_changed/__init__.py
+++ vedro_git_changed/__init__.py
@@ -1,5 +1,5 @@
 from ._git_changed_plugin import VedroGitChanged, VedroGitChangedPlugin
 from ._git_repo import GitRepo, GitRepoError
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __all__ = ("VedroGitChanged", "VedroGitChangedPlugin", "GitRepo", "GitRepoError",)
